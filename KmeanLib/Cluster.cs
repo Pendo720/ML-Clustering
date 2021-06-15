eeea48c69bcade18eb56afd8922d64092f580918
Jada-S <pendo720@gmail.com>
2021-06-15 11:35:02 +0100

    library project code

---
+++ KmeanLib/Cluster.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KmsLibrary
+{
+    public sealed class Cluster<T>
+    {
+        public String Label { get; set; }
+
+        public Feature<T> Centroid { get; set; }
+        public List<Feature<T>> Elements { get; set; }
+
+        public float Radius { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="centroid"></param>
+        /// <param name="label"></param>
+        public Cluster(Feature<T> centroid, String label)
+        {
+            Centroid = centroid;
+            Label = label;
+            Elements = new List<Feature<T>>();
+            Radius = 0f;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Cluster()
+        {
+            Centroid = new Feature<T>();
+            Label = "";
+            Elements = new List<Feature<T>>();
+            Radius = 0f;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string toReturn = String.Format("Label: {0}\nCentroid: {1} Elements({2})\n", Label, Centroid, Elements.Count);
+            Elements.Select(m => toReturn += m.ToString()).ToList();
+            return toReturn;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Feature<T> AdjustCentroid()
+        {
+            var target = new Feature<Field>();
+            var fields = new List<Field>();
+            var all = new List<Field>();
+            Elements.ForEach(f => all.AddRange(f.Fields as List<Field>));
+            if (all.Count != 0)
+            {
+                Centroid.Reset();
+                target.Fields.Clear();
+                fields.Clear();
+                all.GroupBy(p => p.Name)
+                    .ToList()
+                    .Select(p => new { Name = p.Key, Value = p.Average(s => s.Value) })
+                    .ToList()
+                    .ForEach(p =>
+                    {
+                        var toAdd = new Field(p.Name, p.Value);
+                        target.Fields.Add(toAdd);
+                    });
+            }
+
+            return target as Feature<T>;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public void UpdateFieldRanges()
+        {
+            if (Elements.Count > 0)
+            {
+                Radius = Centroid.distanceTo(Elements.Aggregate((p, q) => Centroid.distanceTo(p) > Centroid.distanceTo(q) ? p : q));
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="feature"></param>
+        /// <returns></returns>
+        public Feature<T> IsWithin(Feature<T> feature)
+        {
+            if (Radius != 0f)
+            {
+                var where = Centroid.distanceTo(feature);
+                return where < Radius ? Centroid : null;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ KmeanLib/Field.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.Json;
+
+namespace KmsLibrary
+{
+    public sealed class Field
+    {
+        public string Name { get; set; }
+        public float Value { get; set; }
+
+        public Field()
+        {
+            Name = "None";
+            Value = float.MinValue;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        public Field(string name, float value)
+        {
+            Name = name;
+            Value = value;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => String.Format("{0}:{1}", Name, (float)Value);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>        
+        public override bool Equals(object obj) =>
+            ((obj == null || GetType() != obj.GetType()) ? false :
+            Name.Equals((obj as Field).Name) && Value.Equals((obj as Field).Value));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode() => base.GetHashCode();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string asJson() => JsonSerializer.Serialize(this);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fmin"></param>
+        /// <param name="fmax"></param>
+        /// <returns></returns>
+        public float Normalise(float fmin, float fmax)
+        {
+            float f = Value - fmin;
+            Value = (float)((double)f / fmax);
+            return Value;
+        }
+    }
+}
